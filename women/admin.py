15e9ce7c04bfb38cbd5a417418935b85699e4f75
Madi <abaymadi1@gmail.com>
2023-01-24 17:15:27 +0600

    first commit

---
+++ women/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from women.models import Women
+
+
+# Register your models here.
+admin.site.register(model_or_iterable=Women)
---
+++ women/views.py
@@ -0,0 +1,10 @@
+from rest_framework import generics
+
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView(generics.ListAPIView):
+
+    queryset = Women.objects.all()
+    serializer_class = WomenSerializer
