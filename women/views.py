a503e2da38624e4343e315320211da48123596ce
Madi <abaymadi1@gmail.com>
2023-01-26 12:06:47 +0600

    APIView

--- women/views.py
+++ women/views.py
@@ -1,10 +1,22 @@
+from django.forms import model_to_dict
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Women
 from .serializers import WomenSerializer
 
 
-class WomenAPIView(generics.ListAPIView):
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': lst})
+    
+    def post(self, request):
+        post_new = Women.objects.create(**request.data)
+        return Response({'post': model_to_dict(post_new)})
 
-    queryset = Women.objects.all()
-    serializer_class = WomenSerializer
+# class WomenAPIView(generics.ListAPIView):
+
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
