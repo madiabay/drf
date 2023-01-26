100749e4319646b306414cba5e2403fd9c66fc61
Madi <abaymadi1@gmail.com>
2023-01-26 13:04:48 +0600

    продолжение последнего коммита (decode)

--- women/serializers.py
+++ women/serializers.py
@@ -1,5 +1,7 @@
+import io
 from rest_framework import serializers
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from .models import Women
 
 
@@ -18,7 +20,16 @@ class WomenSerializer(serializers.Serializer):
 # object -> dict -> json
 def encode():
     model = WomenModel(title='Madi', content='Madi Abay')
-    model_sr = WomenSerializer(model)
+    model_sr = WomenSerializer(instance=model)
     print(model_sr.data, type(model_sr.data), sep='++++++')
     json = JSONRenderer().render(model_sr.data)
     print(json)
+
+# json -> dict -> object
+def decode():
+    stream = io.BytesIO(b'{"title":"Madi","content":"Madi Abay"}')
+    data = JSONParser().parse(stream=stream)
+    serializer = WomenSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data)
+    return serializer.validated_data
