b34bba1ea7a657f8b0c8ca544a90b5dfb7b63839
Madi <abaymadi1@gmail.com>
2023-01-26 12:40:57 +0600

    Как сериализатор работает под капотом

--- women/serializers.py
+++ women/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
 from .models import Women
 
 
-class WomenSerializer(serializers.ModelSerializer):
+class WomenModel:
 
-    class Meta:
-        model = Women
-        fields = '__all__'
+    def __init__(self, title, content) -> None:
+        self.title = title
+        self.content = content
+
+
+class WomenSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+
+
+# object -> dict -> json
+def encode():
+    model = WomenModel(title='Madi', content='Madi Abay')
+    model_sr = WomenSerializer(model)
+    print(model_sr.data, type(model_sr.data), sep='++++++')
+    json = JSONRenderer().render(model_sr.data)
+    print(json)
